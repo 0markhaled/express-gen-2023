5d0ab7ac3698a597ee85398cf9a57e9cf681e825
Omar Mostafa <omark3936@gmail.com>
2023-03-22 10:08:53 -0400

    lab2

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {   //each rout is used by adding "/" after the localhost:9000
+  res.render('index', { title: 'Omar' });
+});
+
+router.get('/:msg', function (req, res, next) {
+  res.render('index', { title: req.params.msg });
 });
 
 module.exports = router;
